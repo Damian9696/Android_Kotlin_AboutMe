ccdfdf5c6d0d1b7c5e74f8334f5b1c1646415024
Damian <damian.ogorek.1996@gmail.com>
2020-03-14 18:17:53 +0100

    Created views, logic, added databinding

---
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.android.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneButton.setOnClickListener { addNickName(it) }
+    }
+
+    private fun addNickName(view: View) {
+
+        binding.apply {
+            nicknameText.text = nickname_edit.text.toString()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ app/src/main/java/com/example/android/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package com.example.android.aboutme
+
+/**
+ * Created by Damian on 14.03.2020 17:34
+ */
+data class MyName(var name: String = "", var nickname: String = "")
